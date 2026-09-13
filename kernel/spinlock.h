fdc93074f782a6cc302e8ff994c107934f449fe2
Mecy9 <Mecy9@users.noreply.github.com>
2026-09-13 12:48:53 +0200

    2026 OS design: lock

---
+++ kernel/spinlock.h
@@ -0,0 +1,21 @@
+// Mutual exclusion lock.
+struct spinlock {
+  uint locked;       // Is the lock held?
+
+  // For debugging:
+  char *name;        // Name of lock.
+  struct cpu *cpu;   // The cpu holding the lock.
+#ifdef LAB_LOCK
+  int nts;
+  int n;
+#endif
+};
+
+#ifdef LAB_LOCK
+// Reader-writer lock.
+struct rwspinlock {
+  volatile uint readers;
+  volatile uint writer;
+  volatile uint waiting_writers;
+};
+#endif
