84f530edb3adb1af02c2a2c13d9bd22e4f5d8e02
Mecy9 <Mecy9@users.noreply.github.com>
2026-09-13 12:48:53 +0200

    2026 OS design: traps

---
+++ kernel/sysproc.c
@@ -0,0 +1,139 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+#include "vm.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  kexit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return kfork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return kwait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int t;
+  int n;
+
+  argint(0, &n);
+  argint(1, &t);
+  addr = myproc()->sz;
+
+  if(t == SBRK_EAGER || n < 0) {
+    if(growproc(n) < 0) {
+      return -1;
+    }
+  } else {
+    // Lazily allocate memory for this process: increase its memory
+    // size but don't allocate memory. If the processes uses the
+    // memory, vmfault() will allocate it.
+    if(addr + n < addr)
+      return -1;
+    myproc()->sz += n;
+  }
+  return addr;
+}
+
+uint64
+sys_pause(void)
+{
+  int n;
+  backtrace();
+  uint ticks0;
+
+  argint(0, &n);
+  if(n < 0)
+    n = 0;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+uint64
+sys_sigalarm(void)
+{
+  int interval;
+  uint64 handler;
+
+  argint(0, &interval);
+  argaddr(1, &handler);
+  if(interval < 0)
+    return -1;
+
+  struct proc *p = myproc();
+  p->alarm_interval = interval;
+  p->alarm_handler = handler;
+  p->alarm_elapsed = 0;
+  if(interval == 0)
+    p->alarm_active = 0;
+  return 0;
+}
+
+uint64
+sys_sigreturn(void)
+{
+  struct proc *p = myproc();
+  uint64 saved_a0 = p->alarm_tf.a0;
+
+  memmove(p->trapframe, &p->alarm_tf, sizeof(*p->trapframe));
+  p->alarm_active = 0;
+  return saved_a0;
+}
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kkill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
